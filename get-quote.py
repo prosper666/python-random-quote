f4a68eb0e870761b79bcd789433abd7c9e27a3f0
prosper666 <nucfengpeipei@163.com>
2021-03-16 23:37:50 +1100

    file writing

--- get-quote.py
+++ get-quote.py
@@ -2,10 +2,13 @@ import random
 def primary():
   #print("Keep it logically awesome.")
 
+  f = open("quotes.txt",'a')
+  #f.write('good day makes good mood')  #add a new line
+  f.close()
+
   f = open("quotes.txt")
   quotes = f.readlines()
-  f.close()
-  last = 13
+  last = 14
   rnd = random.randint(0,last)
   rnd2 = random.randint(0, last)
   print(quotes[rnd], end = quotes[rnd2])
